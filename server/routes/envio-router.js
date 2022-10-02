d6fc0f427cbebf300c3a3743931a22f72f7ef84b
Camilo Urrego <ceurrego@bancofalabella.com.co>
2022-10-02 00:03:38 -0500

    añadida ruta obtener envio por id

--- server/routes/envio-router.js
+++ server/routes/envio-router.js
@@ -4,6 +4,7 @@ const EnvioCTRL = require('../controllers/envio-ctrl')
 
 const router = express.Router()
 
+
 router.post('/crearenvio', EnvioCTRL.createEnvio)
 router.put('/envio/:id', EnvioCTRL.updateEnvio)
 router.delete('/envio/:id', EnvioCTRL.deleteEnvio)
